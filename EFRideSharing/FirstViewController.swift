bbaed552947ea3fdc477ccfc42688e245ad12c90
Ilia Tikhomirov <iliatikhomirov@Ilias-MacBook-Air.local>
2015-09-20 01:38:59 -0400

    Initial Commit

---
+++ EFRideSharing/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  EFRideSharing
+//
+//  Created by Ilia Tikhomirov on 20/09/15.
+//  Copyright (c) 2015 Ilia Tikhomirov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
