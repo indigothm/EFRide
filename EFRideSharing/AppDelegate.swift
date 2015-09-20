e5b965fa9a805d832e7ebde0b99d89e8ab9b982d
Elliot T <tikhomirovi@icloud.com>
2015-09-20 15:04:25 -0400

    Create login view UI

--- EFRideSharing/AppDelegate.swift
+++ EFRideSharing/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
     
-        ( window?.rootViewController as! UITabBarController ).tabBar.tintColor =  UIColor(red:0.53, green:0.71, blue:0.91, alpha:1.0)
+     //   ( window?.rootViewController as! UITabBarController ).tabBar.tintColor =  UIColor(red:0.53, green:0.71, blue:0.91, alpha:1.0)
         
         return true
     }
